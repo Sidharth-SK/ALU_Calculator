0da05a6ced07282aa4a8acd3d00490f37683f745
Sidharth S K <87354023+Sidharth-SK@users.noreply.github.com>
2023-10-27 07:34:21 +0530

    Update ALU_Calculator.ino

--- ALU_Calculator/ALU_Calculator.ino
+++ ALU_Calculator/ALU_Calculator.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include  <Keypad.h> //Header file for Keypad from https://github.com/Chris--A/Keypad
 
+
+const int button1 = 8;  // the number of the pushbutton pin
+const int button2 = 9;  // the number of the pushbutton pin
+int button1State = 0;  // variable for reading the pushbutton status
+int button2State = 0;  // variable for reading the pushbutton status
+
+
+void setup() {
+  Serial.begin(9600);  // start serial for output
+  lcd.init(); // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  lcd.setCursor(0, 0); // Set cursor to the first line, first position
+  lcd.print("ALU Calculator");
+  lcd.setCursor(0, 1);
 
+  key = kpd.getKey();  //storing pressed key value in a char
+
+  if (key!=NO_KEY)
+  DetectButtons();
+
+  if  (result==true)
+  CalculateResult();
+
+  DisplayResult();
 }
+
