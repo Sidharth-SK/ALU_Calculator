15ec0a3dd7a739cc2a6856297edbb6b5819f9e61
Sidharth S K <87354023+Sidharth-SK@users.noreply.github.com>
2023-10-27 07:49:14 +0530

    Update ALU_Calculator.ino
    Function that detects and maps the button in the connected 4x4 keypad

--- ALU_Calculator/ALU_Calculator.ino
+++ ALU_Calculator/ALU_Calculator.ino
@@ -49,6 +49,111 @@ void loop() {
   DisplayResult();
 }
 
+void  DetectButtons()
+{ 
+     lcd.clear(); //Then clean it
+    if (key=='*')  //If cancel Button is pressed
+    {Serial.println ("Button Cancel"); Number=Num1=Num2=0;  result=false;}
+    
+     if (key == '1') //If Button 1 is pressed
+    {Serial.println  ("Button 1"); 
+    if (Number==0)
+    Number=1;
+    else
+    Number  = (Number*10) + 1; //Pressed twice
+    }
+    
+     if (key == '4') //If  Button 4 is pressed
+    {Serial.println ("Button 4"); 
+    if (Number==0)
+    Number=4;
+    else
+    Number = (Number*10) + 4; //Pressed twice
+    }
+    
+     if (key == '7') //If Button 7 is pressed
+    {Serial.println ("Button  7");
+    if (Number==0)
+    Number=7;
+    else
+    Number = (Number*10)  + 7; //Pressed twice
+    } 
+  
+
+    if (key == '0')
+    {Serial.println  ("Button 0"); //Button 0 is Pressed
+    if (Number==0)
+    Number=0;
+    else
+    Number = (Number*10) + 0; //Pressed twice
+    }
+    
+     if  (key == '2') //Button 2 is Pressed
+    {Serial.println ("Button 2"); 
+     if  (Number==0)
+    Number=2;
+    else
+    Number = (Number*10) + 2; //Pressed  twice
+    }
+    
+     if (key == '5')
+    {Serial.println ("Button  5"); 
+     if (Number==0)
+    Number=5;
+    else
+    Number = (Number*10)  + 5; //Pressed twice
+    }
+    
+     if (key == '8')
+    {Serial.println  ("Button 8"); 
+     if (Number==0)
+    Number=8;
+    else
+    Number  = (Number*10) + 8; //Pressed twice
+    }   
+  
+
+    if (key == '#')
+    {Serial.println ("Button Equal"); 
+    Num2=Number;
+    result = true;
+    }
+    
+     if (key == '3')
+    {Serial.println ("Button 3"); 
+     if (Number==0)
+    Number=3;
+    else
+    Number = (Number*10) + 3;  //Pressed twice
+    }
+    
+     if (key == '6')
+    {Serial.println  ("Button 6"); 
+    if (Number==0)
+    Number=6;
+    else
+    Number  = (Number*10) + 6; //Pressed twice
+    }
+    
+     if (key == '9')
+    {Serial.println ("Button 9");
+    if (Number==0)
+    Number=9;
+    else
+    Number = (Number*10) + 9; //Pressed twice
+    }
+    if(ButtonPush() == true){
+      lcd.setCursor(0, 1);
+      lcd.print("Arithmetic:- ");
+      arithmetic();
+    }
+    if (ButtonPush() == false){
+      lcd.setCursor(0, 1);
+      lcd.print("Logical:- ");
+      logical();
+    }
+
+}
 
 void CalculateResult() 
 {
